d5f93bfb7e18b15a673385ff6af396516f9cb594
udayk02 <udaykirankarusodi@gmail.com>
2025-03-20 20:47:36 +0530

    added the tagFile command functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { getDiffieHellman } from 'crypto';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	
 	console.log('Congratulations, your extension "tagit" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tagit.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tagit!');
+	const workspaceState = context.workspaceState;
+
+	// async tagFile command
+	const tagFileCommand = vscode.commands.registerCommand('tagit.tagFile', async () => {
+		// currently active text editor
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage("No file is currently open. Please choose a file.");
+			return;	// no open text editor
+		}
+
+		const filePath = editor.document.uri.fsPath;
+		const tagsInput = await vscode.window.showInputBox({
+			prompt: `Enter tags (comma-separated)`,
+			placeHolder: `e.g., #stack, #heap`,
+			value: getFileTags(filePath, workspaceState).join(", ")	// pre fill the input box with existing tags
+		});
+
+		if (tagsInput) {
+			const tags = tagsInput.split(",").map(tag => tag.trim());
+			setFileTags(filePath, tags, workspaceState);			// store the tags into the workspace
+			console.log(`File: ${filePath}, Tags: ${tags}`);
+
+			vscode.window.showInformationMessage(`Tags added to the current file.`);
+		} else {
+			vscode.window.showInformationMessage(`Tagging cancelled.`);
+		}
+
+		vscode.window.showInformationMessage('Tagging this file.');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(tagFileCommand);
 }
+
+/**
+ * Gets the tags associated with a given file
+ * @param filePath The absolute path of the current file
+ * @param workspaceState VSCode's workspace state memento
+ * @returns An array of tags associated with the file
+ */
+function getFileTags(filePath: string, workspaceState: vscode.Memento) : string[] {
+	const tags = workspaceState.get<string[]>(filePath);
+	return tags || [];	// return empty array if no tags are found
+} 
+
+/**
+ * Sets tags to a given file
+ * @param filePath The absolute path of the current file
+ * @param tags The tags to be added to the file
+ * @param workspaceState VSCode's workspace state memento
+ */
+function setFileTags(filePath: string, tags: string[], workspaceState: vscode.Memento) {
+	workspaceState.update(filePath, tags);
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
